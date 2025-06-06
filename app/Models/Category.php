e4909b7332a9e73b01011d75c927868d9e725703
hafizandrean <hafizandreansrg@gmail.com>
2025-06-06 21:44:15 +0700

    commit

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'category';
+
+    protected $guarded = ['id'];
+
+    public function lostItem()
+    {
+        return $this->hasMany(Lost::class, 'category_id');
+    }
+
+    public function foundItem()
+    {
+        return $this->hasMany(Found::class, 'category_id');
+    }
+}

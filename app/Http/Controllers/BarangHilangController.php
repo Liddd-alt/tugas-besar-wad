e32b9b33997769cb0271eb402b95ddb8811ff99f
zielgithub <118146352+zielgithub@users.noreply.github.com>
2025-06-06 23:54:42 +0700

    Create BarangHilangController.php

---
+++ app/Http/Controllers/BarangHilangController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lost;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;

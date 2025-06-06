33f8311b2219157b6efec91e98c2ad2000144511
zielgithub <zielfaqih007@gmail.com>
2025-06-07 00:14:57 +0700

    Update BarangHilangController.php

--- app/Http/Controllers/BarangHilangController.php
+++ app/Http/Controllers/BarangHilangController.php
@@ -7,3 +7,24 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+
+class LostController extends Controller
+{
+    public function index()
+    {
+        if (Auth::user()->role === 'admin') {
+            $losts = Lost::with('category', 'user')->latest()->get();
+        } else {
+            $losts = Lost::with('category', 'user')
+                ->where('user_id', Auth::id())
+                ->latest()
+                ->get();
+        }
+        return view('lost.index', compact('losts'));
+    }
+
+    public function create()
+    {
+        $categories = Category::all();
+        return view('lost.create', compact('categories'));
+    }
